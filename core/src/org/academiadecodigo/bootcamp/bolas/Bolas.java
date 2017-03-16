183597db7b187437728ddd572c8266d675f295fb
João Chapa <reischapa@gmail.com>
2017-03-16 22:10:36 +0000

    PHYSICS FOLLOWS THE SPRITES

--- core/src/org/academiadecodigo/bootcamp/bolas/Bolas.java
+++ core/src/org/academiadecodigo/bootcamp/bolas/Bolas.java
@@ -9,10 +9,12 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 import org.academiadecodigo.bootcamp.bolas.gameobjects.Platform;
 import org.academiadecodigo.bootcamp.bolas.state.GameStateManager;
 import org.academiadecodigo.bootcamp.bolas.state.PlayingState;
+import org.academiadecodigo.bootcamp.bolas.state.testingstates.PlatformCollisionTestingState;
 import org.academiadecodigo.bootcamp.bolas.state.testingstates.PlatformTestingState;
 
 public class Bolas extends Game {
@@ -20,17 +22,18 @@ public class Bolas extends Game {
 	Texture img;
 
 	GameStateManager manager;
-	World world;
 
-	
+
+
 	@Override
 	public void create () {
 
 		Box2D.init();
 
+
 		batch = new SpriteBatch();
 		this.manager = new GameStateManager();
-		this.manager.push(new PlatformTestingState(this.manager));
+		this.manager.push(new PlatformCollisionTestingState(this.manager));
 	}
 
 	@Override
@@ -49,7 +52,7 @@ public class Bolas extends Game {
 
 
 	private void clearScreen() {
-		Gdx.gl.glClearColor(0, 0, 0, 0);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 	}
 
--- core/src/org/academiadecodigo/bootcamp/bolas/gameobjects/Platform.java
+++ core/src/org/academiadecodigo/bootcamp/bolas/gameobjects/Platform.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
+import org.academiadecodigo.bootcamp.bolas.state.testingstates.PlatformCollisionTestingState;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +27,8 @@ public class Platform {
     private float x;
     private float y;
 
+    private float rotation;
+
     private float speed;
 
     public Platform(float x, float y, float width, float height, World world ) {
@@ -35,43 +38,56 @@ public class Platform {
         this.y = y;
 
         this.sprite = new Sprite(new Texture(Gdx.files.internal("core/assets/images/bar.png")));
-        this.sprite.setSize(this.width, this.height);
-        this.sprite.setPosition(this.x, this.y);
+        this.sprite.setSize(this.width * PlatformCollisionTestingState.CONV, this.height * PlatformCollisionTestingState.CONV);
+        this.sprite.setPosition(this.x * PlatformCollisionTestingState.CONV, this.y * PlatformCollisionTestingState.CONV);
         this.initializePhysics(world);
     }
 
     private void initializePhysics(World world) {
         // First we create a body definition
         BodyDef bodyDef = new BodyDef();
-
-        // We set our body to dynamic, for something like ground which doesn't move we would set it to StaticBody
         bodyDef.type = BodyDef.BodyType.DynamicBody;
-
-        // Set our body's starting position in the world
         bodyDef.position.set(this.x, this.y);
+
 
         this.body = world.createBody(bodyDef);
 
         PolygonShape shape = new PolygonShape();
-        shape.setAsBox(this.width, this.height, new Vector2(this.x, this.y), 0);
+        shape.setAsBox(0.5f * this.width, 0.5f * this.height);
 
         FixtureDef fixtureDef = new FixtureDef();
         fixtureDef.shape = shape;
         fixtureDef.density = 1f;
         body.createFixture(fixtureDef);
+
+
     }
 
     public void render(SpriteBatch batch) {
-        this.sprite.translate(this.body.getPosition().x - this.x, this.body.getPosition().y - this.y);
-        sprite.draw(batch);
+
+
+//        this.x = this.body.getPosition().x;
+//        this.y = this.body.getPosition().y;
+
+//        this.sprite.translate(this.body.getPosition().x- this.x, this.body.getPosition().y - this.y);
+
+        this.sprite.setPosition(this.x  - this.width * 0.5f , this.y - this.height * 0.5f );
+        System.out.println(this.rotation);
+        this.sprite.setRotation(this.rotation);
+        this.sprite.draw(batch);
+
         this.x = this.body.getPosition().x;
         this.y = this.body.getPosition().y;
+        this.rotation = this.body.getAngle();
+
         System.out.println(this.body.getPosition());
+//        System.out.println(this.sprite.getX() + " " + this.sprite.getY());
     }
 
 
     public void setSpeed(float newSpeedX, float newSpeedY) {
         this.body.setLinearVelocity(newSpeedX,newSpeedY);
+//        this.body.applyForceToCenter(newSpeedX * 1000, newSpeedY * 1000, true);
     }
 
     public boolean isOutsideBoundaries(int x0, int y0, int xf, int yf) {
@@ -79,11 +95,9 @@ public class Platform {
                 this.body.getPosition().x < x0 || this.body.getPosition().y < y0;
     }
 
-
     public void dispose() {
-        //TODO ????
+        this.sprite.getTexture().dispose();
     }
-
 
 
 }
